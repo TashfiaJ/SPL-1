21a22bbc97fa861f407f5ae38defcc16fa3d5176
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2022-02-16 21:49:35 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+string command;
+
+void create_table()
+{
+
+}
+
+int main ()
+{
+    cout << "Enter command: ";
+    cin >> command;
+
+    if(command == "CREATE")
+    {
+        create_table();
+    }
+}
