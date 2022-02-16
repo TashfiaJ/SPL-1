f13ff90bb9fa3da8ff90cb96ae2ec6e067968ee8
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2022-02-17 05:33:31 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,23 @@ string command, table, TABLENAME, str;
 
 void create_table()
 {
+    cin >> table >> TABLENAME >> str;
+    ofstream database;
+    database.open("My_database.txt");
+    database << TABLENAME << endl;
 
+    if(str=="(")
+    {
+        while(str!=")")
+        {
+            string var_type;
+            cin >> str >> var_type;
+            if(str!=")")
+            database << str << "\t";
+        }
+        cout << endl;
+    }
+    database.close();
 }
 
 int main ()
