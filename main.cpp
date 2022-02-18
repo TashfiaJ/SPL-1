12fefab15dfe0bf6454f3a77a368ba906b14d840
Tashfia Jannath <76718979+TashfiaJannath@users.noreply.github.com>
2022-02-18 19:57:29 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 string command, table, TABLENAME, str;
 
+struct ARRAY {
+    string table_name;
+    ll row;
+    ll column;
+    };
+
+
 /* CREATE TABLE table (
    ID int,
    NAME varchar,
@@ -29,6 +36,60 @@ void create_table()
     }
     database.close();
 }
+
+/* INSERT INTO table name
+   values ( 1, Tash, 1223 ) ;
+*/
+void insert_table ()
+{
+    string into, tablename, value, str;
+    
+    // input into table name values (
+    cin >> into;
+    cin >> tablename;
+    cin >> value;
+    cin >> str;
+    
+    // table name, total row, column row store from file
+    // first need to store total table no so open the file
+    ifstream ifile;
+    
+    ifile.open("FileInformation.txt", ios::in);
+    
+    ll total_table = 0;
+    ifile >> total_table;
+    
+    // close the file
+    ifile.close();
+    
+    //if there is no table then show this message
+    if ( total_table == 0)
+    {
+        cout << " Can't insert the values as there is no table. First create a table\n";
+    }
+    
+    else
+    {
+        ifstream ifile;
+    
+        ifile.open("FileInformation.txt", ios::in);
+        
+        ifile >> total_table;
+        
+        ARRAY tableinfo[total_table];
+        
+        //storing table name, total row, total column
+        
+        for(ll i=0; i<total_table; i++)
+        {
+            ifile >> tableinfo[i].table_name;
+            ifile >> tableinfo[i].row;
+            ifile >> tableinfo[i].column;
+        }
+    }
+    
+}
+
 int main ()
 {
     cout << "Enter the command: ";
@@ -37,5 +98,9 @@ int main ()
     if(command == "CREATE")
     {
         create_table();
+    }
+    else if (command == "INSERT")
+    {
+        insert_table();
     }
 }
