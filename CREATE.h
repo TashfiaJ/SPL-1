42e30ae3f735be93d77f7221a4d0fd5456ec4f35
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-03-04 12:39:35 +0600

    Update CREATE.h

--- CREATE.h
+++ CREATE.h
@@ -3,6 +3,25 @@
 using namespace std;
 #define ll long long
 
+struct ARRAY {
+    string table_name;
+    ll row;
+    ll column;
+    };
+
+string *HEADER;
+string *FIELD;
+
+void column_name (ll x)
+{
+    HEADER = new string [x];
+}
+
+void field_value(ll x)
+{
+    FIELD = new string [x];
+}
+
 /* CREATE TABLE table (
    ID int,
    NAME varchar,
@@ -53,4 +72,49 @@ void createtable(string command, string table, string TABLENAME, string str)
 
        cout << endl << endl << "Table created successfully!" << endl << endl;
     }
+     else
+    {
+        ifstream ifile;
+
+        ifile.open("FileInformation.txt", ios::in);
+
+        ifile >> total_table;
+
+        ARRAY tableinfo[total_table];
+
+        //storing table name, total row, total column
+
+        for(ll i=0; i<total_table; i++)
+        {
+            ifile >> tableinfo[i].table_name;
+            ifile >> tableinfo[i].row;
+            ifile >> tableinfo[i].column;
+        }
+
+        ifile.close();
+
+        remove("FileInformation.txt");
+        
+        ofstream ofile;
+
+        ofile.open("FileInformation.txt", ios::app);
+
+        ofile << total_table+1 << '\n';
+
+        for(ll i=0; i<total_table; i++)
+        {
+           
+            ofile << tableinfo[i].table_name << '\t';
+            ofile << tableinfo[i].row << '\t';
+            ofile << tableinfo[i].column << " ";
+            ofile << endl;
+            
+        }
+        
+        ll sum_of_rows = 0;
+
+        for (ll i=0; i<total_table; i++)
+        {
+            sum_of_rows += tableinfo[i].row;
+        }
 }
