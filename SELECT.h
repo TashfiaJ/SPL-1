2ff8f197e9333559f8e8a44e68d7ba99e0e2281b
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-04-23 15:55:22 +0600

    asterisk sign as input and show every column
    It gives error. Don't show column name

--- SELECT.h
+++ SELECT.h
@@ -61,3 +61,124 @@ void selectInfo()
         {
             cout<<"Can't Select. Create a table  first."<<endl;
         }
+                else
+        {
+            ifstream iInfo;
+            iInfo.open("info.txt", ios::in);
+            iInfo>>numOfTables;
+
+            structArr(numOfTables);
+
+            for(int i=0;i<numOfTables;i++)
+            {
+                iInfo>>strArr[i].tableName;
+                iInfo>>strArr[i].numOfRows;
+                iInfo>>strArr[i].numCol;
+            }
+
+            iInfo.close();
+
+            remove("info.txt");
+
+            ofstream oInfo;
+            oInfo.open("info.txt", ios::app);
+            oInfo<<numOfTables<<endl;;
+
+            for(int i=0;i<numOfTables;i++)
+            {
+                oInfo<<strArr[i].tableName<<" "<<strArr[i].numOfRows<<" "<<strArr[i].numCol<<endl;
+            }
+
+                oInfo.close();
+
+                int sum=0;
+
+                for(int i=0;i<numOfTables;i++)
+                {
+                    sum+=strArr[i].numOfRows;
+                }
+
+                createStringArr(sum);
+
+                if(sum==0)
+                    createHeaderArray(2);
+                else
+                    createHeaderArray(sum);
+
+                string tableName,s;
+
+                ifstream dFile;
+                dFile.open("database.txt", ios::in);
+
+                int idx=0;
+
+                for(int i=0;i<numOfTables;i++)
+                {
+                    dFile>>tableName;
+                    getline(dFile,s);
+                    getline(dFile,s);
+                    getline(dFile,header[i]);
+                    getline(dFile,s);
+
+                    int individualIndexTable=0;
+
+                    while(individualIndexTable!=strArr[i].numOfRows)
+                    {
+                        getline(dFile,stringArr[idx]);
+                        idx++;
+                        individualIndexTable++;
+                    }
+
+                    if(i!=numOfTables-1)
+                    {
+                        getline(dFile,s);
+                    }
+                }
+
+                dFile.close();
+
+                remove("database.txt");
+
+                ofstream aFile;
+                aFile.open("database.txt", ios::app);
+
+                idx=0;
+
+                for(int i=0;i<numOfTables;i++)
+                {
+                    int backUpIdx=idx;
+                    if(strArr[i].tableName==s3)
+                    {
+                        cout<<endl<<endl<<strArr[i].tableName<<endl<<endl;
+                        cout<<header[i]<<endl<<endl;
+
+                        int individualIndexTable=0;
+
+                        while(individualIndexTable!=strArr[i].numOfRows)
+                        {
+                            cout<<stringArr[idx]<<endl;
+                            individualIndexTable++;
+                            idx++;
+                        }
+                    }
+
+                    idx=backUpIdx;
+
+                    aFile<<strArr[i].tableName<<endl<<endl;
+                    aFile<<header[i]<<endl<<endl;
+
+                    int individualIndexTable=0;
+
+                    while(individualIndexTable!=strArr[i].numOfRows)
+                    {
+                        aFile<<stringArr[idx]<<endl;
+                        idx++;
+                        individualIndexTable++;
+                    }
+
+                    if(i!=numOfTables-1)  aFile<<endl;
+                }
+
+                aFile.close();
+        }
+    }
